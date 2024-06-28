b5f8c2adb3bae0eb9f4badbd55282ca5e8319236
Kshitij <65331304+kshitij1235@users.noreply.github.com>
2024-06-28 14:34:23 +0530

    new changes`

--- src/components/TitleScreen.tsx
+++ src/components/TitleScreen.tsx
@@ -7,7 +7,10 @@ import Globe from "./magicui/Globe";
 function TitleScreen() {
     return (
         <Fragment>
-            <Particles color="black" size={0.1}></Particles>
+            <Particles color="black"  quantity={200}></Particles>
+
+            <Particles color="black" size={0.3} ></Particles>
+
             <div style={{ height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", flexDirection: "column" }}>
                 <div style={{ fontFamily: "monospace", fontWeight: "normal", fontSize: "20px", marginBottom: "5px" }}>
                     <BlurIn word="HI , I AM KSHITIJ JATHAR" duration={1}></BlurIn>
@@ -15,7 +18,7 @@ function TitleScreen() {
                 <div style={{ textAlign: "center", fontFamily: "monospace", fontWeight: "normal", fontSize: "14px" }}>
                     <BlurIn word="AM 22 , AND I LIKE TO CODE !!" duration={2}></BlurIn>
                 </div>
-            <CustomButton />
+            {/* <CustomButton /> */}
             </div>
         </Fragment>
     );
